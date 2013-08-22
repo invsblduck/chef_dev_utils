75589ecc8323415cf8df9b582d5eef1a01550c7d
invsblduck <invsblduck@gmail.com>
2013-08-22 13:37:27 -0700

    upload bugfix

--- knife/bootstrap/rebundle_bootstrap.sh
+++ knife/bootstrap/rebundle_bootstrap.sh
@@ -9,4 +9,5 @@ real_script=$(stat -c %N $script |sed 's/^.* -> //' |sed "s/'//g")
 tar -C $(dirname $real_script) -cvzhf $TARBALL bootstrap
 source ~/.swiftrc  # path required for non-interactive shell
 echo uploading...
+cd /tmp # swift will pick up the tmp/ subdir otherwise
 swift upload $CONTAINER $TARBALL
