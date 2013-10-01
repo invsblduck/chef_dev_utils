386e7083624fa61fb2a95f60cbeb88c7efbe3e62
invsblduck <invsblduck@gmail.com>
2013-10-01 11:40:35 -0700

    changed default chef url to 'https://chef'

--- vm_kick/knife/bootstrap/bootstrap/.chef/knife.rb
+++ vm_kick/knife/bootstrap/bootstrap/.chef/knife.rb
@@ -2,4 +2,4 @@ log_level                :info
 log_location             STDOUT
 validation_client_name   'chef-validator'
 validation_key           '/root/bootstrap/chef-validator.pem'
-chef_server_url          'https://bc-chef'
+chef_server_url          'https://chef'
