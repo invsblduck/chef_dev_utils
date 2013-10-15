60d0dff1735e237daad2986685ded8e7ff23b688
invsblduck <invsblduck@gmail.com>
2013-10-15 11:42:37 -0700

    new script to merge changes from upstream remote

---
+++ rcb/rcb_git_merge_upstream.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+valid_branches=(master grizzly icehouse)
+
+b=`git symbolic-ref HEAD`
+current_branch=${b##*/}
+
+# 
+# Check that current branch is what we want
+#
+if [ -z "$current_branch" ]; then
+    echo "could not determine current git branch"
+    exit 1
+fi
+
+valid=
+for branch in "${valid_branches[@]}"; do
+    [ $branch = $current_branch ] && valid=1 && break
+done
+
+if [ -z "$valid" ]; then
+    echo "your current branch is not one of: ${valid_branches[*]}"
+    echo "please stash your work and checkout one of those branches first."
+    exit 1
+fi
+
+#
+# Fetch the upstream commits and merge them
+#
+# TODO verify remote 'upstream' exists
+set -e
+git fetch -v upstream
+
+if [ $(git diff $current_branch upstream/$current_branch |wc -l) != 0 ]; then
+    read -n1 -p "view the diffs? [Y/n]: "
+    if [[ "${REPLY:-y}" =~ [yY] ]]; then
+        git -p diff $current_branch upstream/$current_branch
+    fi
+
+    read -n1 -p "merge commits? [Y/n]: "
+    if [[ "${REPLY:-y}" =~ [yY] ]]; then
+        git merge upstream/$current_branch
+    fi
+fi
