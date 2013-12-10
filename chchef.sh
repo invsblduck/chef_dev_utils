f878eb84ec20e643c03202bf977599d672d70e77
invsblduck <invsblduck@gmail.com>
2013-12-09 23:14:05 -0800

    show current server when invoked w/o args

--- chchef.sh
+++ chchef.sh
@@ -10,8 +10,16 @@ _chchef() {
  
 chchef() {
     local server=${1}
+
+    if [ -z "${server}" ]; then
+        basename $(readlink ~/.chef/chef_server_url)
+        return 0
+    fi
     
-    [ ! -e ${HOME}/.chef/servers.d/${server} ] && return 1
+    if [ ! -e ${HOME}/.chef/servers.d/${server} ]; then
+        return 1
+    fi
+
     ln -sf ${HOME}/.chef/servers.d/${server} ${HOME}/.chef/chef_server_url
     cat ${HOME}/.chef/chef_server_url
 }
