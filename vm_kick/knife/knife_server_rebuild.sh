7db22ab1128046cf5e2942c6f40238ac37d8814b
invsblduck <invsblduck@gmail.com>
2014-02-12 13:15:17 -0800

    distro is now passed in as ubuntu-precise
    for fakecloud support.  don't break public cloud image logic.

--- vm_kick/knife/knife_server_rebuild.sh
+++ vm_kick/knife/knife_server_rebuild.sh
@@ -8,18 +8,22 @@ else
     distro=${2:-ubuntu}
 fi
 
-if [ "$distro" = "ubuntu" ]; then
-    #IMAGE=6a668bb8-fb5d-407a-9a89-6f957bced767     # ubuntu 12.04 (20130529)
-    #IMAGE=23b564c9-c3e6-49f9-bc68-86c7a9ab5018     # ubuntu 12.04 (20130713)
-    IMAGE=25de7af5-1668-46fb-bd08-9974b63a4806      # ubuntu 12.04 (20131021)
-    TEMPL=~/.chef/bootstrap/bc-ubuntu.erb
-elif [ "$distro" = "centos" ]; then
-    IMAGE=25a5f2e8-f522-4fe0-b0e0-dbaa62405c25      # centos 6.4 
-    TEMPL=~/.chef/bootstrap/bc-centos.erb
-else
-    echo "${0##*/}: '$distro' not a supported distro!" >&2
-    exit 1
-fi
+case $distro in
+    ubuntu*)
+        #IMAGE=6a668bb8-fb5d-407a-9a89-6f957bced767   # ubuntu 12.04 (20130529)
+        #IMAGE=23b564c9-c3e6-49f9-bc68-86c7a9ab5018   # ubuntu 12.04 (20130713)
+        IMAGE=25de7af5-1668-46fb-bd08-9974b63a4806    # ubuntu 12.04 (20131021)
+        TEMPL=~/.chef/bootstrap/bc-ubuntu.erb
+    ;;
+    centos*)
+        IMAGE=25a5f2e8-f522-4fe0-b0e0-dbaa62405c25    # centos 6.4
+        TEMPL=~/.chef/bootstrap/bc-centos.erb
+    ;;
+    *)
+        echo "${0##*/}: '$distro' not a supported distro!" >&2
+        exit 1
+    ;;
+esac
 
 # we don't need error handling around node name because
 # the following command does the node logic for us.
